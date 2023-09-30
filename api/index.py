a2556e7044783b9cea47636b8a6f0270c9c7eb66
Nickerz100 <38448436+Nickerz100@users.noreply.github.com>
2023-09-30 14:26:01 -0500

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,46 @@
+import requests
+
+
+
+def getPublicIPInfo():
+
+    # API endpoint 
+
+    apiURL = "http://ip-api.com/json/"
+
+
+
+    # GET request for IP address parsing
+
+    response = requests.get(apiURL)
+
+    ipInfo = response.json()
+
+
+
+    # Extract relevant information
+
+    ip = ipInfo['query']
+
+    city = ipInfo['city']
+
+    country = ipInfo['country']
+
+    isp = ipInfo['isp']
+
+
+
+    # Display IP information
+
+    print("Public IP Address: " + ip)
+
+    print("City: " + city)
+
+    print("Country: " + country)
+
+    print("ISP: " + isp)
+
+
+
+getPublicIPInfo()
+
