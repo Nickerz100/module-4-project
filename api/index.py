c769b485382a5b4f041dc3741b2257842bd3da68
Nickerz100 <38448436+Nickerz100@users.noreply.github.com>
2023-09-30 17:08:15 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,33 +1,23 @@
 import requests
 
-def get_public_ip_info():
-    # Define the API endpoint
-    api_url = 'http://ip-api.com/json/'
+def getPublicIPInfo():
+    # API endpoint 
+    apiURL = "http://ip-api.com/json/"
 
-    try:
-        # Make a GET request to the API
-        response = requests.get(api_url)
+    # GET request for IP address parsing
+    response = requests.get(apiURL)
+    ipInfo = response.json()
 
-        # Check if the request was successful (HTTP status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response
-            ip_info = response.json()
+    # Extract relevant information
+    ip = ipInfo['query']
+    city = ipInfo['city']
+    country = ipInfo['country']
+    isp = ipInfo['isp']
 
-            # Extract relevant information
-            ip = ip_info['query']
-            city = ip_info['city']
-            country = ip_info['country']
-            isp = ip_info['isp']
+    # Display IP information
+    print("Public IP Address: " + ip)
+    print("City: " + city)
+    print("Country: " + country)
+    print("ISP: " + isp)
 
-            # Display the formatted information
-            print(f"Public IP Address: {ip}")
-            print(f"City: {city}")
-            print(f"Country: {country}")
-            print(f"ISP: {isp}")
-        else:
-            print("Failed to retrieve IP information.")
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-if __name__ == "__main__":
-    get_public_ip_info()
+getPublicIPInfo()
